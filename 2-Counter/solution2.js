a53d33d8a09d4a722ec188a5b799f20d3524f612
Prince NZAMUWE(Fudji Bokande) <134228300+princenzmw@users.noreply.github.com>
2024-01-02 22:44:39 +0200

    Code for solution2

---
+++ 2-Counter/solution2.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function (n) {
+  let counter = n;
+
+  return function () {
+    const currentCounter = counter;
+    counter++;
+
+    return currentCounter;
+  };
+};
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
